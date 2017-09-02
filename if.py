f2017141b6724797da2ee95ee906ad92add6102b
John Pan <galvatronus2001@gmail.com>
2017-09-01 19:39:37 -0700

    reduce

--- if.py
+++ if.py
@@ -1,4 +1,4 @@
-a = input()
+a = input("please enter your age:")
 
 if a > 12:
     print("adult")
---
+++ reduce.py
@@ -0,0 +1,5 @@
+def add(x,y):
+    return x + y  
+#print add(3,4)
+
+print reduce(add, [1,3,5,7,9,11])
