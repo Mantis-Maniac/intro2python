be8a8688e95889cb14c0ead4406ab636cc77cd97
John Pan <galvatronus2001@gmail.com>
2017-09-03 14:34:08 -0700

    nothing

---
+++ filter.py
@@ -0,0 +1,6 @@
+def is_odd(n):
+    return n % 2 == 1
+
+#print is_odd(3)
+
+print filter(is_odd, range(100))
---
+++ hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+' a test module'
+
+__author__ = 'Mantis'
+
+import sys
+
+def test():
+    print 'Hello world'
+
+#if __name__ == '__main__':
+#    test()
